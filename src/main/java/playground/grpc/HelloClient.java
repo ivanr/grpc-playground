5f74cc566ec0af6ed9531a5d7353777eb7640f6b
Ivan Ristic <ivanr@webkreator.com>
2021-07-02 17:03:35 +0100

    Move invocations into separate methods.

--- src/main/java/playground/grpc/HelloClient.java
+++ src/main/java/playground/grpc/HelloClient.java
@@ -1,6 +1,7 @@
 package playground.grpc;
 
 import com.google.protobuf.Any;
+import com.google.protobuf.InvalidProtocolBufferException;
 import com.google.rpc.BadRequest;
 import io.grpc.ManagedChannel;
 import io.grpc.ManagedChannelBuilder;
@@ -24,13 +25,10 @@ public class HelloClient {
         this.asyncStub = HelloGrpc.newStub(channel);
     }
 
-    public static void main(String[] args) throws Exception {
-        HelloClient client = new HelloClient("localhost", HelloServer.SERVER_PORT);
-
-        // Example of a blocking invocation and a successful response.
-
+    // Example of a blocking invocation and a successful response.
+    private void successfulBlockingRequest() {
         try {
-            HelloResponse response = client.blockingStub.sayHello(
+            HelloResponse response = blockingStub.sayHello(
                     HelloRequest.newBuilder()
                             .setName("Ivan")
                             .build());
@@ -38,11 +36,12 @@ public class HelloClient {
         } catch (StatusRuntimeException e) {
             System.out.println("Request #1: Error: " + e.getMessage());
         }
+    }
 
-        // In this example, we don't give our name and the service responds with an error.
-
+    // In this example, we don't give our name and the service responds with an error.
+    private void failedBlockingRequest() {
         try {
-            HelloResponse response = client.blockingStub.sayHello(
+            HelloResponse response = blockingStub.sayHello(
                     HelloRequest.newBuilder()
                             // .setName("Ivan")
                             .build());
@@ -63,12 +62,22 @@ public class HelloClient {
             com.google.rpc.Status status = io.grpc.protobuf.StatusProto.fromThrowable(e);
             for (Any any : status.getDetailsList()) {
                 if (any.is(BadRequest.class)) {
-                    BadRequest br = any.unpack(BadRequest.class);
-                    for (BadRequest.FieldViolation fv : br.getFieldViolationsList()) {
-                        System.out.println("Violation: " + fv.getField() + ": " + fv.getDescription());
+                    try {
+                        BadRequest br = any.unpack(BadRequest.class);
+                        for (BadRequest.FieldViolation fv : br.getFieldViolationsList()) {
+                            System.out.println("Violation: " + fv.getField() + ": " + fv.getDescription());
+                        }
+                    } catch (InvalidProtocolBufferException ipbe) {
+                        // TODO Shouldn't happen but log if it does.
                     }
                 }
             }
         }
+    }
+
+    public static void main(String[] args) throws Exception {
+        HelloClient client = new HelloClient("localhost", HelloServer.SERVER_PORT);
+        client.successfulBlockingRequest();
+        client.failedBlockingRequest();
     }
 }
